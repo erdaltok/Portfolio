7a20e6429d17d1ec791a9ee869602a7055641645
Erdal Tok <erdaltok@hotmail.de>
2024-06-14 18:05:02 +0200

    skills in production

--- src/app/main-content/content-components/content-components.component.ts
+++ src/app/main-content/content-components/content-components.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ReferencesComponent } from './references/references.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-content-components',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, ReferencesComponent, AboutMeComponent],
+  imports: [CommonModule, LandingPageComponent, ReferencesComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './content-components.component.html',
   styleUrl: './content-components.component.scss'
 })
