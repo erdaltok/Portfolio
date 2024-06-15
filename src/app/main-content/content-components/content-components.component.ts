5eab6abe48d617beba01880fe28ff8e86f3118f4
Erdal Tok <erdaltok@hotmail.de>
2024-06-15 22:40:26 +0200

    skills section include ngFor

--- src/app/main-content/content-components/content-components.component.ts
+++ src/app/main-content/content-components/content-components.component.ts
@@ -4,11 +4,12 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ReferencesComponent } from './references/references.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioProjectsComponent } from './portfolio-projects/portfolio-projects.component';
 
 @Component({
   selector: 'app-content-components',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, ReferencesComponent, AboutMeComponent, SkillsComponent],
+  imports: [CommonModule, LandingPageComponent, ReferencesComponent, AboutMeComponent, SkillsComponent, PortfolioProjectsComponent],
   templateUrl: './content-components.component.html',
   styleUrl: './content-components.component.scss'
 })
--- src/app/main-content/content-components/skills/skills.component.ts
+++ src/app/main-content/content-components/skills/skills.component.ts
@@ -10,4 +10,62 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+ngOnInit(): void {
+  
+   
+  } 
+
+
+
+
+
+logos = [
+  {
+    name: 'Angular',
+    path: '../../../../assets/img/icons/Angular.png'
+  },
+  {
+    name: 'TypeScript',
+    path: '../../../../assets/img/icons/Typescript.png'
+  },
+  {
+    name: 'JavaScript',
+    path: '../../../../assets/img/icons/Javascript.png'
+  }, 
+  {
+    name: 'HTML',
+    path: '../../../../assets/img/icons/html.png'
+  },
+  {
+    name: 'Firebase',
+    path: '../../../../assets/img/icons/Firebase.png'
+  },
+  {
+    name: 'GIT',
+    path: '../../../../assets/img/icons/Git.png'
+  },
+  {
+    name: 'CSS',
+    path: '../../../../assets/img/icons/CSS.png'
+  },
+  {
+    name: 'Rest-Api',
+    path: '../../../../assets/img/icons/Api.png'
+  },
+  {
+    name: 'Scrum',
+    path: '../../../../assets/img/icons/scrum.png'
+  },
+  {
+    name: 'Material design',
+    path: '../../../../assets/img/icons/Material-design.png'
+  }
+]
+
+
+
+
+
+
+
 }
