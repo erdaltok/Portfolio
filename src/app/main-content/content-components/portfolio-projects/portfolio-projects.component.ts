1df65c585f795b375130d028a823cce6af8a8152
Erdal Tok <erdaltok@hotmail.de>
2024-06-19 01:54:32 +0200

    portfolio section include ngFor

--- src/app/main-content/content-components/portfolio-projects/portfolio-projects.component.ts
+++ src/app/main-content/content-components/portfolio-projects/portfolio-projects.component.ts
@@ -11,6 +11,37 @@ import { Component } from '@angular/core';
 export class PortfolioProjectsComponent {
 
 
+  projects = [
+    {
+      name: 'join',
+      skills: 'JavaScript | HTML | CSS | Git',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      image: '../../../../assets/img/project-images/join-img2.png',
+      liveTestLink: '',
+      gitLink: 'https://github.com/erdaltok/Join-753'
+    },
+
+    {
+      name: 'El Pollo Loco',
+      skills: 'JavaScript | HTML | CSS | Git',
+      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and poison bottles to fight against the killer chicken.',
+      image: '../../../../assets/img/project-images/el-pollo-loco-img.png',
+      liveTestLink: '',
+      gitLink: 'https://github.com/erdaltok/El-pollo-loco'
+    }
+    
+  ]
+  
+
+openLiveTestProject(url:string) {
+  window.open(url, '_blank')
+}
+openGitProject(url:string) {
+  window.open(url, '_blank')
+}
+
+
+
 
 
 
