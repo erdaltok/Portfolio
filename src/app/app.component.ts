d90bfaaa5edea029d7d142a0cc36f4e969e6ba33
Erdal Tok <erdaltok@hotmail.de>
2024-06-19 04:00:34 +0200

    contact section add send message btn and footer design ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { HeaderComponent } from './main-content/header/header.component';
+import { FooterComponent } from './main-content/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
