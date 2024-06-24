0791328f9b63194fc82d8cc0affc30c603b5b132
Erdal Tok <erdaltok@hotmail.de>
2024-06-25 01:50:39 +0200

    include burger menu navbar

--- src/app/main-content/header/header.component.ts
+++ src/app/main-content/header/header.component.ts
@@ -9,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
+  menuValue: boolean = false;
+  menuIcon: string = '';
+  currentImgSrc = "../../../assets/img/icons/burger-icon-open.png";
+
+
+
+  openMenu() {
+    this.menuValue = !this.menuValue;
+    if (this.menuValue) {
+      this.currentImgSrc = "../../../assets/img/icons/burger-icon-close.png";
+    } else {
+      this.currentImgSrc = "../../../assets/img/icons/burger-icon-open.png";
+    }
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeMenu() {
+    this.menuValue = false;
+    this.currentImgSrc = "../../../assets/img/icons/burger-icon-open.png"
+    document.body.style.overflow = 'auto';
+
+}
+
 }
