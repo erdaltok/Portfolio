0517d0fb16af7e7f0752162729d49a6dc6f95ec2
Erdal Tok <erdaltok@hotmail.de>
2024-07-25 16:36:29 +0200

    bug fixes

--- src/app/main-content/footer/footer.component.ts
+++ src/app/main-content/footer/footer.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, TranslateModule, HttpClientModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public translateService: TranslateService) {
       
     }
 
--- src/app/main-content/imprint/imprint.component.ts
+++ src/app/main-content/imprint/imprint.component.ts
@@ -1,16 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, TranslateModule, HttpClientModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+
+  constructor(public translateService: TranslateService) {}
 
   currentImgSrc = "../../../../assets/img/icons/go-up-btn-white.png";
 
